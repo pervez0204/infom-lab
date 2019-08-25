1434b95d92fc18f23493efb3b6b776d938f81a0c
pervez0204 <sanjida.shuchi06@gmail.com>
2019-08-25 10:40:56 +0600

    First commit

---
+++ CT.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+print("hello Info");
+}
+
+
+
+
+
+quit
+
+
+
+
+
+
---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<< "Hello world!";
+retun 0;
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout << "hello World!";
+ return 0;
+}
